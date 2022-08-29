2810a8f7910ad6f1f216010d258fca8f403ff5b4
Seth <seth.bbrendle@gmail.com>
2022-08-29 17:28:39 -0400

    commit tree 3b4478538193411c10bde0de0a4e9d97c23318ad

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result;
+  
+  if (feet <= 400) {
+    result = "This one is on me!"
+  } else if (feet > 400 && feet < 2000) {
+    result = "That will be twenty bucks."
+  } else if (feet > 2000 && feet < 2500) {
+    result = "I will gladly take your thirty bucks."
+  } else if (feet >= 2500) {
+    result = "No can do."
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+scuberGreetingForFeet(2501); 
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous": 
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+       return "Thank you.";
+    default:
+       return "Bye."
+  }
 }
